0f0cfb2c20d401574a0a63967298bca4af38c87c
Kuprys, Petras <Petras.Kuprys@cmegroup.com>
2013-05-19 18:53:27 -0500

    generated app

---
+++ who_is_the_boss/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WhoIsTheBoss::Application.config.secret_token = 'a386f6af56cd80f9d6180066f282a8e482a57885690ef29dbd3d8dfaaad204dc33cfebf27eaccd848e294c8792b23abb4108cde763c594ff309217f231b2b4f7'
